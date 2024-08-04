0e7defe47c4f612a803e7d36910412e28bdb77a2
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-08-04 12:52:57 +0300

    added score to the Game Over Screen

--- Fly-Fly Sviny/scenes/GamePlayScreen/GameOverScreen.gd
+++ Fly-Fly Sviny/scenes/GamePlayScreen/GameOverScreen.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var game_over_timer:Timer = $GameOverTimer
+@onready var game_over_timer: Timer = $GameOverTimer
+@onready var score_value: Label = $GameOverScreenContainer/MarginContainer/VBoxContainer/Score
 
 
 
@@ -8,6 +9,7 @@ extends Control
 func _ready():
 	hide()
 	SignalManager.on_sviny_destroyed.connect(on_sviny_destroyed)
+	
 
 
 func _on_game_over_timer_timeout():
@@ -16,4 +18,8 @@ func _on_game_over_timer_timeout():
 
 func on_sviny_destroyed() -> void:
 	game_over_timer.start()
+	show_high_score()
 	show()
+
+func show_high_score() -> void:
+	score_value.text = str(ScoreManager.get_high_score())
